2d0453b2cf2181c556211c691853839be2bb0bb3
DrDiHai <sven.kindlimann@gmail.com>
2024-01-20 16:47:52 +0100

    Cleanup and Comments

--- src/app/ConnectedComponent/ConnectedComponent.component.ts
+++ src/app/ConnectedComponent/ConnectedComponent.component.ts
@@ -7,6 +7,12 @@ import { ConnectionService } from '../connection-service.service';
     templateUrl: './ConnectedComponent.component.html',
     styleUrls: ['./ConnectedComponent.component.css']
 })
+
+/// This class is the base class for all components that need to send and receive messages to and from the backend.
+/// It provides methods for sending messages and subscribing to incoming messages.
+/// Subclasses should implement the handleMessages method to handle incoming messages, and the handleError method to handle errors.
+/// If necessary, they can also implement the handleComplete method to handle the connection closing.
+/// They can also call the sendMessage method to send messages.
 export class ConnectedComponent implements OnInit, OnDestroy {
 
     constructor(private connectionService: ConnectionService) {
@@ -15,10 +21,12 @@ export class ConnectedComponent implements OnInit, OnDestroy {
     private componentID: string = '';
     private connection!: Observable<any>;
 
+    // Sends a message to the backend.
     sendMessage(message: Object) {
         this.connectionService.sendMessage(this.componentID, message);
     }
 
+    /// This method closes the connection to the backend when the component is destroyed.
     ngOnDestroy(): void {
         this.connectionService.removeConnection(this.componentID);
     }
@@ -28,14 +36,18 @@ export class ConnectedComponent implements OnInit, OnDestroy {
         throw new Error('Subclasses must implement the handleMessages method.');
     }
 
+    // Abstract method for components to implement their error handling.
     handleError(error: any): void {
         throw new Error('Subclasses must implement the handleError method.');
     }
 
+    // Abstract method for components to implement their connection closing handling.
     handleComplete(): void {
         console.warn(`Connection closed for component ${this.componentID}.`);
     }
 
+    // Creates the connection to the backend when the component is initialized.
+    // Subclasses should call super.ngOnInit() in their ngOnInit method instead creating their own connection.
     ngOnInit() {
         const [connection, componentID] = this.connectionService.getNewConnection();
         this.connection = connection;
--- src/app/connection-service.service.ts
+++ src/app/connection-service.service.ts
@@ -5,6 +5,12 @@ import { WebSocketSubject } from 'rxjs/webSocket';
 @Injectable({
     providedIn: 'root'
 })
+
+
+/// This Service manages all WebSocket connections to the backend.
+/// It provides new connections to components that need them, and manages the connections.
+/// Components should call the getNewConnection method to get a new connection.
+/// They should also call the removeConnection method when they are done with the connection.
 export class ConnectionService {
 
     constructor() { }
@@ -13,6 +19,8 @@ export class ConnectionService {
     private connections: { [componentId: string]: WebSocketSubject<any> } = {};
     componentCounter: number = 0;
 
+    // Create a new connection and return it.
+    // Users of the connection must provide the returned componentID when sending messages.
     getNewConnection(): [Observable<any>, string] {
         let connection = new WebSocketSubject<any>(this.BACKEND_ADDRESS);
         const componentID = this.generateComponentId();
@@ -20,6 +28,7 @@ export class ConnectionService {
         return [connection, componentID];
     }
 
+    // Send a message to the backend.
     sendMessage(senderComponentID: string, message: Object) {
         let connection = this.connections[senderComponentID];
         connection.next(message);
@@ -34,56 +43,10 @@ export class ConnectionService {
         }
     }
     
-    
     // Generate and return a unique component identifier.
     private generateComponentId(): string {
         const componentId = `component-${this.componentCounter}`;
         this.componentCounter++;
         return componentId;
     }
-
-
-
 }
-
-/*
-export class HelloWorldService {
-
-    connection$?: WebSocketSubject<any>;
-    BACKEND_ADDRESS = 'ws://localhost:8765/'
-
-    connectToTest(): Observable<any> {
-        return this.connect(this.BACKEND_ADDRESS);
-    }
-
-    connect(endpoint: string): Observable<any> {
-        // If no connection object has been created yet, return a new connection
-        if (!this.connection$) this.connection$ = webSocket(endpoint);
-        return this.connection$;
-    }
-
-    getHelloWorld(): Observable<any> {
-        if (!this.connection$) {
-            throw new Error('No connection established!');
-        }
-        return this.connection$?.multiplex(
-            () => ('Hello World'),
-            () => ('Goodbye World'),
-            (message) => true
-        )
-    }
-
-    send(data: Object) {
-		console.log("HelloWorldService sending message")
-		console.log(data)
-		if (this.connection$) {
-			this.connection$.next(data);
-		} else {
-			console.error('Connection has not been created, cannot send ' + String(data))
-		}
-	}
-
-    constructor() { }
-
-}
-*/
