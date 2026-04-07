89a93da5701531c953570d3d24a95230e4cb9bd2
hkret <s9i089wd4s.github@happy2.e4ward.co>
2026-04-07 20:51:06 +0200

    improvements

--- src/app/field/relation-field.component.ts
+++ src/app/field/relation-field.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Input, Output, OnInit, OnDestroy } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnDestroy, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ConnectedComponent } from '../connected-component/connected.component';
 import { ConnectionService } from '../connection.service';
 import { IncomingMessage, MessageType } from '../messages/Message';
 import { FetchList, ObjectList, ListObject } from '../messages/data.messages';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 
  type SelectOption = Omit<ListObject, 'id'> & {
     id: number | null;
@@ -24,20 +25,29 @@ export class RelationFieldComponent extends ConnectedComponent implements OnInit
     @Input() schema: any;
     @Output() valueChange = new EventEmitter<SelectOption | null>();
 
-    // Ein Subject, das die Optionen hält
+    @ViewChild('inputElement') inputElement!: ElementRef<HTMLInputElement>;
+
+    // subject providing the full list of options fetched from the backend
     options$ = new BehaviorSubject<SelectOption[]>([]);
+    filterText$ = new BehaviorSubject<string>('');
+    filteredOptions$ = combineLatest([this.options$, this.filterText$]).pipe(
+        map(([options, filter]) => options.filter(opt => 
+            opt.display_name.toLowerCase().includes(filter.toLowerCase())
+        ))
+    );
     isOpen = false;
     isLoading = false;
+    selectedIndex = -1;
+    currentFilteredOptions: SelectOption[] = [];
 
-    // possibleValues: ListObject[] = [];
-    // isLoading = false;    selectedId: string = '';
-    constructor(protected override connectionService: ConnectionService) {
+    constructor(protected override connectionService: ConnectionService, private elementRef: ElementRef) {
         super(connectionService);
         this.setComponentID('RelationFieldComponent');
     }
 
     override ngOnInit() {
         super.ngOnInit();
+        this.filteredOptions$.subscribe(options => this.currentFilteredOptions = options);
     }
 
     override ngOnDestroy() {
@@ -60,6 +70,8 @@ export class RelationFieldComponent extends ConnectedComponent implements OnInit
     toggleOpen() {
         if (this.isOpen) {
             this.isOpen = false;
+            this.filterText$.next('');
+            this.selectedIndex = -1;
             return;
         }
 
@@ -68,18 +80,26 @@ export class RelationFieldComponent extends ConnectedComponent implements OnInit
             this.fetchPossibleValues();
         } else {
             this.isOpen = true;
+            this.selectedIndex = this.currentFilteredOptions.findIndex(opt => 
+                (opt.id === null && !this.value) || (opt.id === this.value?.id)
+            );
+            setTimeout(() => this.inputElement.nativeElement.focus());
         }
     }
 
     override handleMessages(message: IncomingMessage): void {
         console.groupCollapsed(this.componentID, "received", message.type, "message");
-        if (message.type === MessageType.ObjectList) {
+        if (message.type === MessageType.ObjectList  && this.isLoading) {
             console.log(`${this.componentID} handling ObjectList`, message);
             const objectList = message as ObjectList;
             const emptyOption: SelectOption = { id: null, display_name: '--- None ---' };
             this.options$.next([emptyOption, ...objectList.objects]);            
             this.isLoading = false;
             this.isOpen = true;
+            this.selectedIndex = this.currentFilteredOptions.findIndex(opt => 
+                (opt.id === null && !this.value) || (opt.id === this.value?.id)
+            );
+            setTimeout(() => this.inputElement.nativeElement.focus());
         } else {
             console.error(`${this.componentID} handling Unexpected message`, message);
         }
@@ -87,6 +107,7 @@ export class RelationFieldComponent extends ConnectedComponent implements OnInit
     }
 
     selectOption(option: SelectOption) {
+        console.log(`${this.componentID} option selected`, option);
         if (option.id === null) {
             this.value = null;
             this.valueChange.emit(null);
@@ -95,10 +116,52 @@ export class RelationFieldComponent extends ConnectedComponent implements OnInit
             this.valueChange.emit({...option, bo_type: this.boType});
         }
         this.isOpen = false;
+        this.filterText$.next('');
+        this.selectedIndex = -1;
     }
 
     close() {
         this.isOpen = false;
+        this.filterText$.next('');
+        this.selectedIndex = -1;
     }
 
+    onInput(event: Event) {
+        const target = event.target as HTMLInputElement;
+        this.filterText$.next(target.value);
+        this.selectedIndex = -1;
+    }
+
+    onKeyDown(event: KeyboardEvent) {
+        if (event.key === 'ArrowDown') {
+            event.preventDefault();
+            if (this.selectedIndex < this.currentFilteredOptions.length - 1) {
+                this.selectedIndex++;
+            }
+        } else if (event.key === 'ArrowUp') {
+            event.preventDefault();
+            if (this.selectedIndex > 0) {
+                this.selectedIndex--;
+            } else if (this.selectedIndex === 0) {
+                this.selectedIndex = -1;
+            }
+        } else if (event.key === 'Enter') {
+            event.preventDefault();
+            if (this.selectedIndex >= 0 && this.selectedIndex < this.currentFilteredOptions.length) {
+                this.selectOption(this.currentFilteredOptions[this.selectedIndex]);
+            } else if (this.currentFilteredOptions.length === 1) {
+                this.selectOption(this.currentFilteredOptions[0]);
+            }
+        } else if (event.key === 'Escape') {
+            event.preventDefault();
+            this.close();
+        }
+    }
+
+    @HostListener('document:click', ['$event'])
+    onDocumentClick(event: Event) {
+        if (!this.elementRef.nativeElement.contains(event.target as Node)) {
+            this.close();
+        }
+    }
 }
