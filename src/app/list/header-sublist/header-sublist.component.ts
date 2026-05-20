9efdb2fd9afe47b78486b3f614a8bf7cb50c32f9
hkret <s9i089wd4s.github@happy2.e4ward.co>
2026-05-20 16:30:55 +0200

    cleanup removal of  FetchList and ObjectList messages

--- src/app/list/header-sublist/header-sublist.component.ts
+++ src/app/list/header-sublist/header-sublist.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { BoIdentifier } from 'src/app/business-object/bo.identifier';
 import { ConnectedComponent } from 'src/app/connected-component/connected.component';
 import { ConnectionService } from 'src/app/connection.service';
-import { FetchList, FetchMessage, ObjectList, ObjectMessage } from 'src/app/messages/data.messages';
+import { FetchMessage, ObjectMessage } from 'src/app/messages/data.messages';
 import { ListObject } from 'src/app/messages/Message';
 import { IncomingMessage, MessageType } from 'src/app/messages/Message';
 import { SelectedObjectService } from 'src/app/selected-object.service';
